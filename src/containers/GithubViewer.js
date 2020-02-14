931caff57bedf356ed70fb5959aee9f1123d501d
Ian Andrewson <ianandrewson@gmail.com>
2020-02-14 15:54:31 -0800

    implemented reducer, user stored to state

--- src/containers/GithubViewer.js
+++ src/containers/GithubViewer.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../actions/userActions';
+
 
 
 export default function GithubViewer() {
 
-  
+  const dispatch = useDispatch();
+  const handleUser = ({ target }) => {
+    dispatch(setUser(target.value));
+  };
 
   return (
     <>
-      <input type="text" placeholder="username" />
-      <button onClick={handleSubmit}></button>
+      <input type="text" placeholder="username" onChange={handleUser}/>
+      <button >Find User</button>
     </>
   );
 }
