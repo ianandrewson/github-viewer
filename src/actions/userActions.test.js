b292a87f67754185d017c7a832d6d304d7c5a180
Ian Andrewson <ianandrewson@gmail.com>
2020-02-16 20:03:37 -0800

    test written for getting repo pulls action; ready to TDD

--- src/actions/userActions.test.js
+++ src/actions/userActions.test.js
@@ -57,4 +57,23 @@ describe('user action tests', () => {
         });
       });
   });
+
+  it('creates an action to fetch a repos pull requests', () => {
+    const dispatch = jest.fn();
+    const action = getRepoPulls('repo-name');
+    
+    return action(dispatch)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: FETCH_REPO_PULLS_LOADING
+        });
+        expect(dispatch).toHaveBeenCalledWith({
+          action: FETCH_REPO_PULLS,
+          payload: [
+            { number: 1, state: 'open', title: 'best new feature', link: 'blah' },
+            { number: 2, state: 'closed', title: 'other new feature', link: 'blahblah' }
+          ]
+        });
+      });
+  });
 });
