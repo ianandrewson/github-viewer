fc77f6d3f53cf98d1e666aa7ee2d1183ce0ad6da
Ian Andrewson <ianandrewson@gmail.com>
2020-02-16 21:19:08 -0800

    tests written for repo reducer handling fetch repo pulls, and associated loading. Ready to TDD reducer

---
+++ src/reducers/repoReducer.test.js
@@ -0,0 +1,26 @@
+import { FETCH_REPO_PULLS_LOADING, FETCH_REPO_PULLS } from '../actions/repoActions';
+
+describe('repo reducer tests', () => {
+  it('can handle the FETCH_REPO_PULLS_LOADING action', () => {
+    const initialState = { repoPulls: [], loading: false };
+    const action = { type: FETCH_REPO_PULLS_LOADING };
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ repoPulls: [], loading: true });
+  });
+
+  it('can handle the FETCH_REPO_PULLS action', () => {
+    const initialState = { repos: [], loading: true };
+    const action = { 
+      type: FETCH_REPO_PULLS,
+      payload: [
+        { number: 1, state: 'open', title: 'best new feature', link: 'blah' },
+        { number: 2, state: 'closed', title: 'other new feature', link: 'blahblah' }]
+    };
+    const newState = reducer(initialState, action);
+    expect(newState).toEqual({ loading: false, repoPulls: [
+      { number: 1, state: 'open', title: 'best new feature', link: 'blah' },
+      { number: 2, state: 'closed', title: 'other new feature', link: 'blahblah' }
+    ] });
+  });
+});
